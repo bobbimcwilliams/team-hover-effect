e0b5985976cb97e8ad648418d6acc3cb2ff8a561
bobbimcwilliams <bobbimc@att.net>
2025-06-16 11:29:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-const icon = document.getElementById("webCifar-icon");
-const sidebox = document.getElementById("webCifar");
-const close = document.querySelector(".close");
-
-icon.addEventListener("click", () => {
-  sidebox.classList.add("active");
-});
-
-close.addEventListener("click", () => {
-  sidebox.classList.remove("active");
+new hoverEffect({
+  parent: document.querySelector('.img1'),
+  intensity: 0.3,
+  image1: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d',
+  image2: 'https://images.unsplash.com/photo-1499084732479-de2c02d45fc4',
+  displacementImage: 'https://raw.githubusercontent.com/robin-dela/hover-effect/master/images/heightMap.png',
+  imagesRatio: 800 / 1200
 });
